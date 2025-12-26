9aab65daacb024c5a85fb38e31b13ca56144b52b
demidelarue-lang <demidelarue@gmail.com>
2025-12-26 18:00:51 +0400

    Add files via upload

---
+++ Travel Blog/main.js
@@ -0,0 +1,15 @@
+function wrong1(){
+    document.getElementById("button1").style.backgroundColor="red";
+}
+function wrong2(){
+    document.getElementById("button2").style.backgroundColor="red";
+}
+function right1(){
+    document.getElementById("button3").style.backgroundColor="green";
+}
+function wrong3(){
+    document.getElementById("button4").style.backgroundColor="red";
+}
+function right2(){
+    document.getElementById("button5").style.backgroundColor="green";
+}
